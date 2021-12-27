6e0da265c56cd0639bbd83b12ecf89955943a2bf
PorthoGamesBR <matheusrporto2009@gmail.com>
2021-12-27 18:38:03 -0300

    First route

--- run.py
+++ run.py
@@ -1,1 +1,14 @@
-from flask import Flask
+from flask import Flask, request
+
+app = Flask.init(__name__)
+
+@app.route("/whats")
+def whats_test():
+    return "Hello Whatsauto"
+
+
+
+if __name__ == "__main__":
+    app.run(port=5000)
+
+
