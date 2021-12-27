6ecc1c485ad01614ad19fbd75c4ba79e510df04c
PorthoGamesBR <matheusrporto2009@gmail.com>
2021-12-27 19:35:30 -0300

    Converting whatsauto to string

--- run.py
+++ run.py
@@ -2,17 +2,21 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/whats", methods=['POST'])
+@app.route("/whats", methods=['GET','POST'])
 def whats_test():
-    request_data = request.get_json()
+    #request_data = request.get_json()
+    #request.data.decode('utf-8')
     
-    message = request_data['message']
-    print(message)
+    print(request.get_data().decode('utf-8'))
+    print(request.data.decode('utf-8'))
+    
+    #message = request_data['message']
+    #print(message)
     return "Hello Whatsauto"
 
 
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run(debug=True,host="0.0.0.0",port=5000)
 
 
