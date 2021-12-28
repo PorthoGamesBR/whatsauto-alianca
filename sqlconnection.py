cb62d38f1c8c71c60d52b296f7d369edfd65ac6a
PorthoGamesBR <matheusrporto2009@gmail.com>
2021-12-28 20:19:03 -0300

    Create table listona on the database

--- sqlconnection.py
+++ sqlconnection.py
@@ -6,6 +6,7 @@ from getpass import getpass
 connection_data = open('.connectiondata', 'r')
 password = connection_data.readline().replace('\n','')
 host = connection_data.readline().replace('\n','')
+database_name = connection_data.readline().replace('\n','')
 
 config = {
     'user' : 'root',
@@ -14,9 +15,35 @@ config = {
     'client_flags' : [ClientFlag.SSL],
     'ssl_ca' : 'server-ca.pem',
     'ssl_cert' : 'client-cert.pem',
-    'ssl_key' : 'client-key.pem'
-    
+    'ssl_key' : 'client-key.pem',
+    'database' : database_name
 } 
 
 cnxn = mysql.connector.connect(**config)
+
+#Intelisense dont work here
+cursor = cnxn.cursor()
+
+db_table_name = "listona"
+
+cursor.execute("SHOW TABLES LIKE '" + db_table_name + "'")
+result = cursor.fetchone()
+
+#Create first table. Should only be done one time
+if not result:
+    create_table = """
+    CREATE TABLE """ + db_table_name + """(
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        nome VARCHAR(100),
+        nivel INT,
+        xp INT,
+        gold INT
+    )
+
+    """
+    cursor.execute(create_table)
+    cnxn.commit()
+    
+cursor.close()
+
 
